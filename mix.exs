b660656ed3179eedecaa4e267014c594d3ac2cf7
Marten/Qqwy <w-m@wmcode.nl>
2020-06-17 19:49:23 +0200

    Setting up Travis CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blocked.MixProject do
   def project do
     [
       app: :blocked,
-      version: "0.1.0",
+      version: "0.9.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
