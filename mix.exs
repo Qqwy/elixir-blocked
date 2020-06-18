3d92a5fc9d98ccee0e7b552f6fed21a1d717da6d
Marten/Qqwy <w-m@wmcode.nl>
2020-06-18 16:07:28 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blocked.MixProject do
   def project do
     [
       app: :blocked,
-      version: "0.9.1",
+      version: "0.10.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
