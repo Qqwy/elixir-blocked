2630f1c161b06e978b6cd341be42bb80f175f537
Marten/Qqwy <w-m@wmcode.nl>
2020-06-18 19:29:11 +0200

    Configure a public token in the test environment,
    to make the CI rate-limit higher.

--- config/test.exs
+++ config/test.exs
@@ -2,4 +2,7 @@ use Mix.Config
 
 # Run Blocked in the test-environment,
 # even though we're not in a CI.
-config :blocked, [warn: true]
+config :blocked, [
+  warn: true,
+  github_api_token: "39f288d7c45d6f82c55a23e117ead0c2c9f35192"
+]
